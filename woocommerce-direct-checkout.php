69acc8074dcf0dc2740895cbf3d0bb697129c099
Timothy Wood <tim@imaginesimplicity.com>
2014-09-18 19:39:01 -0400

    init plugin file

---
+++ woocommerce-direct-checkout.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Woocommerce: Direct To Checkout
+Plugin URI: 
+Description: Set a product to go directly to checkout without hitting the cart
+Author: Timothy Wood @codearachnid
+Version: 1.0
+Author URI: 
+*/
